ee7e95d5fb220a4c127c6f8a06742b6139755ac2
Giulia Ghisini <giulia.ghisini@redturtle.it>
2024-04-17 13:45:17 +0200

    fix: bundle

--- src/components/manage/index.js
+++ src/components/manage/index.js
@@ -3,17 +3,22 @@ import loadable from '@loadable/component';
 /*--------------------------------
 --- BUNDLE VoltoFeedbackManage ---
 ---------------------------------*/
-export const VFPanel = loadable(() =>
-  import(/* webpackChunkName: "VoltoFeedbackManage" */ './VFPanel/VFPanel'),
+export const VFPanel = loadable(
+  () =>
+    import(/* webpackChunkName: "VoltoFeedbackManage" */ './VFPanel/VFPanel'),
 );
-export const VFPanelMenu = loadable(() =>
-  import(/* webpackChunkName: "VoltoFeedbackManage" */ './VFPanel/VFPanelMenu'),
+export const VFPanelMenu = loadable(
+  () =>
+    import(
+      /* webpackChunkName: "VoltoFeedbackManage" */ './VFPanel/VFPanelMenu'
+    ),
 );
-export const FeedbackComments = loadable(() =>
-  import(
-    /* webpackChunkName: "VoltoFeedbackManage" */ './VFPanel/FeedbackComments'
-  ),
+export const FeedbackComments = loadable(
+  () =>
+    import(
+      /* webpackChunkName: "VoltoFeedbackManage" */ './VFPanel/FeedbackComments'
+    ),
 );
-export const VFToolbar = loadable(() =>
-  import(/* webpackChunkName: "VoltoFeedbackManage" */ './VFToolbar'),
+export const VFToolbar = loadable(
+  () => import(/* webpackChunkName: "VoltoFeedbackView" */ './VFToolbar'), //questo è stato messo in VoltoFeedbackView altrimenti se lo mettiamo in VoltoFeedbackManage viene giu tutto il bundle VoltoFeedbackManage anche da anonimo, perchè questo componente è caricato con appExtras per il path ''
 );
